d75475fc9a8e6c6f252cb4e205aa26e71c506713
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-03-01 20:19:07 +0900

    feat: @Secured 어노테이션 활성화

--- src/main/java/com/ll/security1/controller/IndexController.java
+++ src/main/java/com/ll/security1/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.ll.security1.controller;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,6 +61,13 @@ public class IndexController {
 	@ResponseBody
 	public String joinProc() {
 		return "회원가입 완료됨";
+	}
+
+	@Secured("ROLE_ADMIN") // 간단하게 권한 검사 하고 싶을 때는 Secured 어노테이션 사용
+	@GetMapping("/info")
+	@ResponseBody
+	public String info() {
+		return "개인정보";
 	}
 
 }
